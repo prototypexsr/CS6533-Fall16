06421c579e62baec11cadbd26a1139d24d56eb67
Stefan N. Cherubin <scherubin@nyu.edu>
2016-10-24 12:43:54 -0400

    Some clean up and documentation

--- A2/windows/main.cpp
+++ A2/windows/main.cpp
@@ -1,3 +1,8 @@
+/*Stefan Cherubin HW2
+Primitives used: 2 cubes, 1 sphere
+sphere is inside a cube, which is inside a larger cube
+*/
+
 #include "glsupport.h" //include this file first
 #include "matrix4.h"
 #include "quat.h"
@@ -55,13 +60,7 @@ void display(void) {
 	Matrix4 modelViewMatrix2 = inv(eyeMatrix) * quatToMatrix(combined2);
 
 
-	//Matrix4 modelViewMatrix; 
-	//modelViewMatrix = normalMatrix;
-	//GLfloat glmatrix[16];
-	//modelViewMatrix.writeToColumnMajorMatrix(glmatrix);
-	////normalMatrix.writeToColumnMajorMatrix(glmatrix);
-	//glUniformMatrix4fv(modelviewMatrixUniformLocation, 1, false, glmatrix);
-	//glUniformMatrix4fv(normalUniformLocation, 1, false, glmatrix);
+	//Should move matrix stuff inside Entity class
 	Matrix4 projectionMatrix;
 	projectionMatrix = projectionMatrix.makeProjection(90.0, 1.0, -0.1, -100.0); //vertical field of view is important (1st parameter)! higher vfov will allow for better perephial vision
 	//Vertical Field of View = 2 * arctan( tan(hfox/2) * Aspect Ratio)
@@ -69,13 +68,7 @@ void display(void) {
 	projectionMatrix.writeToColumnMajorMatrix(glmatrixProjection);
 	glUniformMatrix4fv(projectionMatrixUniformLocation, 1, false, glmatrixProjection);
 
-	/*Matrix4 normalMatrix;
-	normalMatrix = transpose(inv(modelViewMatrix));
-	GLfloat glmatrixNormal[16];
-	normalMatrix.writeToColumnMajorMatrix(glmatrixNormal);
-	glUniformMatrix4fv(normalUniformLocation, 1, false, glmatrixNormal);*/
-
-	/*glUniform3f(colorUniformLocation, 0.0, 0.45, 1.0);*/
+	//For now, all three entites will share everything, except for color attribute
 	cube.Draw(modelViewMatrix, positionAttribute, normalAttribute, modelviewMatrixUniformLocation, normalUniformLocation, colorUniformLocation, 0.0, 0.45, 1.0);
 	ball.Draw(modelViewMatrix , positionAttribute, normalAttribute, modelviewMatrixUniformLocation, normalUniformLocation, colorUniformLocation, 1.0, 0.45, 0.0);
 	bigCube.Draw(modelViewMatrix, positionAttribute, normalAttribute, modelviewMatrixUniformLocation, normalUniformLocation, colorUniformLocation, 0.1, 0.2, 0.4);
@@ -94,7 +87,7 @@ void display(void) {
 	glEnableVertexAttribArray(normalAttribute);
 
 	
-
+	//glDrawArrays replaced by glDrawElement in Geometry class
 	//glDrawArrays(GL_TRIANGLES, 0, 36);
 	glDisableVertexAttribArray(positionAttribute);
 	glDisableVertexAttribArray(colorAttribute);
@@ -133,21 +126,12 @@ void init() {
 	projectionMatrixUniformLocation = glGetUniformLocation(program, "projectionMatrix");
 
 
-	/*Entity cube;
-	int ibLen, vbLen;
-	getCubeVbIbLen(vbLen, ibLen);
-	std::vector<VertexPN> vtx(vbLen);
-	std::vector<unsigned short> idx(ibLen);
-	makeCube(2, vtx.begin(), idx.begin());
-	cube.geometry.indexBO = ibLen;
-	cube.geometry.vertexBO = vbLen;
-	cube.geometry.numIndices = ibLen;*/
-	//cube.geometry.Draw(positionAttribute, normalAttribute);
-	//cube.child = &bigCube;
-	//bigCube.child = &ball;
-	cube.adoption(&bigCube);
-	bigCube.adoption(&ball);
-	//getCubeVbIbLen(vbLen2, ibLen2);
+	
+	//cube.adoption(&bigCube);
+	//bigCube.adoption(&ball);
+	bigCube.adoption(&cube);
+	cube.adoption(&ball);
+	
 	getCubeVbIbLen(vbLen, ibLen);
 	cube.geometry.indexBO = ibLen;
 	cube.geometry.vertexBO = vbLen;
@@ -159,7 +143,7 @@ void init() {
 	makeCube(1, vtx.begin(), idx.begin());
 
 
-	getSphereVbIbLen(20, 20, vbLen2, ibLen2);
+	getSphereVbIbLen(20, 20, vbLen2, ibLen2); //more slices & stacks make it more smooth and circle like
 	ball.geometry.indexBO = ibLen2;
 	ball.geometry.vertexBO = vbLen2;
 	ball.geometry.numIndices = ibLen2;
@@ -181,57 +165,6 @@ void init() {
 
 
 
-
-	/*glGenBuffers(1, &vertexBO);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexPN) * vtx.size(), vtx.data(), GL_STATIC_DRAW);
-	glGenBuffers(1, &indexBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned short) * idx.size(), idx.data(), GL_STATIC_DRAW);
-*/
-	/*glGenBuffers(1, &vertPositionVBO);
-	glBindBuffer(GL_ARRAY_BUFFER, vertPositionVBO);
-
-	GLfloat cubeVerts[] = {
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f
-	};
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);*/
 	glGenBuffers(1, &ball.geometry.vertexBO);
 	glBindBuffer(GL_ARRAY_BUFFER, ball.geometry.vertexBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexPN) * vtx2.size(), vtx2.data(), GL_STATIC_DRAW);
@@ -254,49 +187,6 @@ void init() {
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned short) * idx3.size(), idx3.data(), GL_STATIC_DRAW);
 
 
-	glGenBuffers(1, &colorVBO);
-	glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
-
-	GLfloat cubeColors[] = {
-		0.583f, 0.771f, 0.014f, 1.0f,
-		0.609f, 0.115f, 0.436f, 1.0f,
-		0.327f, 0.483f, 0.844f, 1.0f,
-		0.822f, 0.569f, 0.201f, 1.0f,
-		0.435f, 0.602f, 0.223f, 1.0f,
-		0.310f, 0.747f, 0.185f, 1.0f,
-		0.597f, 0.770f, 0.761f, 1.0f,
-		0.559f, 0.436f, 0.730f, 1.0f,
-		0.359f, 0.583f, 0.152f, 1.0f,
-		0.483f, 0.596f, 0.789f, 1.0f,
-		0.559f, 0.861f, 0.639f, 1.0f,
-		0.195f, 0.548f, 0.859f, 1.0f,
-		0.014f, 0.184f, 0.576f, 1.0f,
-		0.771f, 0.328f, 0.970f, 1.0f,
-		0.406f, 0.615f, 0.116f, 1.0f,
-		0.676f, 0.977f, 0.133f, 1.0f,
-		0.971f, 0.572f, 0.833f, 1.0f,
-		0.140f, 0.616f, 0.489f, 1.0f,
-		0.997f, 0.513f, 0.064f, 1.0f,
-		0.945f, 0.719f, 0.592f, 1.0f,
-		0.543f, 0.021f, 0.978f, 1.0f,
-		0.279f, 0.317f, 0.505f, 1.0f,
-		0.167f, 0.620f, 0.077f, 1.0f,
-		0.347f, 0.857f, 0.137f, 1.0f,
-		0.055f, 0.953f, 0.042f, 1.0f,
-		0.714f, 0.505f, 0.345f, 1.0f,
-		0.783f, 0.290f, 0.734f, 1.0f,
-		0.722f, 0.645f, 0.174f, 1.0f,
-		0.302f, 0.455f, 0.848f, 1.0f,
-		0.225f, 0.587f, 0.040f, 1.0f,
-		0.517f, 0.713f, 0.338f, 1.0f,
-		0.053f, 0.959f, 0.120f, 1.0f,
-		0.393f, 0.621f, 0.362f, 1.0f,
-		0.673f, 0.211f, 0.457f, 1.0f,
-		0.820f, 0.883f, 0.371f, 1.0f,
-		0.982f, 0.099f, 0.879f, 1.0f
-	};
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeColors), cubeColors, GL_STATIC_DRAW);
 
 
 	glGenBuffers(1, &normalVBO);
